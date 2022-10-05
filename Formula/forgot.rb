b3490ba9eb8150c817035c6beabca62f75b9967a
OlshaMB <68439653+OlshaMB@users.noreply.github.com>
2022-10-05 11:02:53 +0000

    fix: Hash

--- Formula/forgot.rb
+++ Formula/forgot.rb
@@ -3,7 +3,7 @@ class Forgot < Formula
     desc "A todo list app that indexes your app to find TODO:'s written in rust"
     homepage "https://github.com/OlshaMB/forgot"
     url "https://github.com/OlshaMB/forgot/releases/download/v0.2.0/forgot-macos-latest-x86-64.tar"
-    sha256 "6e05e1a908afd5a8cad7a5838a94550f70b798c374aaa554894790e09cde1c93"
+    sha256 "d221f0c308a0253fb1d2eccd9976fc35d01ed412c93eeb5e9a6e7ea475271f48"
 
     def install
       bin.install "./bundle/forgot"
